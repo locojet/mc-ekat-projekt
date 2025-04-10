a60d4f1c92c675fc164d5d471446a1cf5ba98b69
locojet <72513021+locojet@users.noreply.github.com>
2025-04-10 16:57:59 +0200

    Automatisiertes Update

---
+++ src/components/CookiesInfo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Más Información sobre Cookies</h1>
+      <p>Aquí puedes agregar la información detallada sobre las cookies que se usan en tu sitio.</p>
+      <!-- Agrega más detalles según sea necesario -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CookieInfo'
+  };
+  </script>
+  
+  <style scoped>
+  /* Puedes agregar estilos específicos para esta página */
+  </style>
--- src/components/Dienstleistungen.vue
+++ src/components/Dienstleistungen.vue
@@ -5,137 +5,122 @@
         <h2 class="text-3xl font-bold tracking-tight -mt-6 text-gray-900 sm:text-4xl text-center">Zerstörungsfreie Werkstoffprüfung (ZFP/NDT)</h2>
         <p class="mt-0 text-lg leading-8 text-gray-600 text-center sm:-mb-5">Entdecken Sie unsere Prüfverfahren zur Werkstoffprüfung ohne Materialbeschädigung.</p>
       </div>
-      <div class=" mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-9 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <div class="mx-auto mt-0 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-6 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-9 lg:mx-0 lg:max-w-none lg:grid-cols-3">
 
         <!-- Ultraschalldickenmessungen -->
-        <article class="flex flex-col  text-justify m-5 pt-0">
+        <article class="flex flex-col text-justify m-5 pt-0">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Ultraschall</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Ultraschall</a>
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6">
-              <a href="#">
-                Ultraschalldickenmessungen
-              </a>
+              <a href="#">Ultraschalldickenmessungen</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/Verfahren/ultraschall.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">Thickness Measurement ohne Zerstörung von Beschichtungen.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
 
         <!-- Schweißnahtprüfungen UT -->
         <article class="flex flex-col text-justify m-5">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Ultraschall</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Ultraschall</a>
           </div>
           <div class="group relative">
-            <h3 class="mt-3  text-lg font-semibold leading-6">
-              <a href="#">
-                Schweißnahtprüfungen UT
-              </a>
+            <h3 class="mt-3 text-lg font-semibold leading-6">
+              <a href="#">Schweißnahtprüfungen UT</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/Verfahren/Schweissnahtprufung.png" alt="Schweißnahtprüfungen UT" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">UT für Schweißnähte zur Erkennung von inneren Unregelmäßigkeiten.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
 
         <!-- Magnetpulverprüfungen MP -->
-        <article class="flex flex-col  text-justify m-5">
+        <article class="flex flex-col text-justify m-5">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Magnetisch</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Magnetisch</a>
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6">
-              <a href="#">
-                Magnetpulverprüfungen MP
-              </a>
+              <a href="#">Magnetpulverprüfungen MP</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/Verfahren/mt.png" alt="Magnetpulverprüfungen MP" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">MP zum Auffinden von Oberflächendefekten in ferromagnetischen Materialien.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
 
         <!-- Wirbelstromprüfung ET -->
-        <article class="flex flex-col  text-justify m-5">
+        <article class="flex flex-col text-justify m-5">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Elektrisch</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Elektrisch</a>
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6">
-              <a href="#">
-                Wirbelstromprüfung ET
-              </a>
+              <a href="#">Wirbelstromprüfung ET</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/wirbelstrompruefung.png" alt="Wirbelstromprüfung ET" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">ET zur Erkennung von Oberflächen- und geringfügigen Suboberflächendefekten in leitfähigen Materialien.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
 
         <!-- Visualisierungsprüfung VT -->
-        <article class="flex flex-col items-center justify-center text-justify m-5">
+        <article class="flex flex-col text-justify m-5">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Optisch</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Optisch</a>
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6">
-              <a href="#">
-                Visualisierungsprüfung VT
-              </a>
+              <a href="#">Visualisierungsprüfung VT</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/Verfahren/vt.png" alt="Visualisierungsprüfung VT" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">VT zur direkten Beobachtung von Oberflächen, um Defekte oder Eigenschaften zu identifizieren.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
 
         <!-- Eindringprüfungen PT -->
-        <article class="flex flex-col items-center justify-center text-justify m-5">
+        <article class="flex flex-col text-justify m-5">
           <div class="flex items-center justify-center gap-x-4 text-xs">
-            <a href="#" class=" relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Chemisch</a>
+            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Chemisch</a>
           </div>
           <div class="group relative">
             <h3 class="mt-3 text-lg font-semibold leading-6">
-              <a href="#">
-                Eindringprüfungen PT
-              </a>
+              <a href="#">Eindringprüfungen PT</a>
             </h3>
+            <div class="relative mt-4">
+              <img src="../assets/Verfahren/pt.png" alt="Eindringprüfungen PT" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
             <p class="mt-5 line-clamp-3 text-sm leading-6">PT nutzt farbige oder fluoreszierende Penetrierflüssigkeit zur Darstellung von Oberflächendefekten.</p>
-            <!-- <div class="relative mt-8">
-              <img src="../assets/ultraschal.png" alt="Ultraschalldickenmessungen" class="object-cover h-48 w-full rounded-lg bg-primary">
-            </div> -->
           </div>
         </article>
-
-       
-
       </div>
     </div>
   </div>
 
-   <!-- Zulassung für NDT-Prüfungen im Offshore-Bereich -->
-   <article class="flex flex-col items-center justify-center text-start m-5 p10 pb-20">
-          <div class="flex items-center justify-center gap-x-4 text-xs mt-[-5rem]" >
-            <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-secondary2 hover:bg-gray-100 ">Offshore</a>
-          </div>
-          <div class="group relative p-6">
-            <h3 class="mt-3 text-lg font-semibold leading-6 text-center">
-              <a href="#">
-                Zulassung für NDT-Prüfungen im Offshore-Bereich
-              </a>
-            </h3>
-            <p class="mt-5 line-clamp-3 text-sm leading-6" >Neu zugelassen für NDT-Prüfungen im Offshore-Bereich, DNV GL Supervisor Level III und II, Operatoren Level I verfügbar.</p>
-            
-          </div>
-        </article>
+  <!-- Zulassung für NDT-Prüfungen im Offshore-Bereich -->
+  <article class="flex flex-col items-center justify-center text-start m-5 p10 pb-20">
+    <div class="flex items-center justify-center gap-x-4 text-xs mt-[-5rem]">
+      <a href="#" class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-secondary2 hover:bg-gray-100">Offshore</a>
+    </div>
+    <div class="group relative p-6">
+      <h3 class="mt-3 text-lg font-semibold leading-6 text-center">
+        <a href="#">Zulassung für NDT-Prüfungen im Offshore-Bereich</a>
+      </h3>
+      <div class="relative mt-4">
+              <img src="../assets/Verfahren/offshore.png" alt="Eindringprüfungen PT" class="object-cover h-48 w-full rounded-lg bg-primary">
+            </div>
+      <p class="mt-5 line-clamp-3 text-sm leading-6">Neu zugelassen für NDT-Prüfungen im Offshore-Bereich, DNV GL Supervisor Level III und II, Operatoren Level I verfügbar.</p>
+    </div>
+  </article>
 </template>
 
 <script setup>
@@ -147,16 +132,12 @@ onMounted(() => {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        // Artikel wird sichtbar und bleibt sichtbar
         entry.target.classList.add('article-visible');
         entry.target.classList.remove('article-hidden');
-        // Beobachten einstellen, nachdem der Artikel sichtbar wird
         observer.unobserve(entry.target);
       }
     });
-  }, {
-    threshold: 0.5
-  });
+  }, { threshold: 0.5 });
 
   articles.value = document.querySelectorAll('article');
   articles.value.forEach(article => {
@@ -172,19 +153,17 @@ onBeforeUnmount(() => {
 });
 </script>
 
-
-
 <style scoped>
-#sec-1{
+#sec-1 {
   max-width: 75%;
   margin: 0 auto;
 }
 @media screen and (max-width: 768px) {
-  #sec-1{
+  #sec-1 {
     max-width: 100%;
   }
 }
-/* Grundlegende Transition für alle Artikel */
+
 article {
   transition: opacity 0.6s ease-out, transform 0.6s ease-out;
 }
@@ -198,9 +177,4 @@ article {
   opacity: 1;
   transform: translateX(0);
 }
-
-
 </style>
-
-
-
